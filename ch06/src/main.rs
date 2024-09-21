4d613f4c4282a69186d7adc75e38bfe9480e06ea
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2024-09-21 13:17:06 +0900

    wip

--- ch06/src/main.rs
+++ ch06/src/main.rs
@@ -4,27 +4,37 @@ use std::iter::Peekable;
 enum Node {
     Char(char),
     Seq(Vec<Node>),
+    Star(Box<Node>),
 }
 
-fn _parse(mut input_it: Peekable<impl Iterator<Item = char>>) -> Option<Node> {
+fn _parse(input_it: &mut Peekable<impl Iterator<Item = char>>) -> Option<Node> {
     let c = input_it.next()?;
 
     let n = Node::Char(c);
 
-    if input_it.peek() == None {
-        return Some(n);
+    match input_it.peek() {
+        Some(c) => {
+            if *c == '*' {
+                input_it.next();
+                return Some(Node::Star(Box::new(n)));
+            }
+        }
+        None => return Some(n),
     }
 
     let mut seq = vec![n];
-    while let Some(next) = input_it.next() {
-        seq.push(Node::Char(next));
+    while let Some(next) = _parse(input_it) {
+        match next {
+            Node::Seq(mut cont) => seq.append(&mut cont),
+            _ => seq.push(next),
+        }
     }
 
     return Some(Node::Seq(seq));
 }
 
 fn parse(input: &str) -> Node {
-    if let Some(n) = _parse(input.chars().peekable()) {
+    if let Some(n) = _parse(&mut input.chars().peekable()) {
         return n;
     }
 
@@ -34,12 +44,34 @@ fn parse(input: &str) -> Node {
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
-    fn test_parse() {
+    fn test_parse_char() {
         assert_eq!(parse("a"), Node::Char('a'));
+    }
+
+    #[test]
+    fn test_parse_seq() {
+        assert_eq!(
+            parse("ab"),
+            Node::Seq(vec![Node::Char('a'), Node::Char('b')])
+        );
         assert_eq!(
             parse("abc"),
             Node::Seq(vec![Node::Char('a'), Node::Char('b'), Node::Char('c')])
+        );
+    }
+
+    #[test]
+    fn test_parse_star() {
+        assert_eq!(parse("a*"), Node::Star(Box::new(Node::Char('a'))));
+        assert_eq!(
+            parse("abc*"),
+            Node::Seq(vec![
+                Node::Char('a'),
+                Node::Char('b'),
+                Node::Star(Box::new(Node::Char('c')))
+            ])
         );
     }
 }
