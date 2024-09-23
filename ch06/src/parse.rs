e37fc60e276c07624f0128f9899d0ec3c6113ec6
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2024-09-23 13:21:25 +0900

    wip

--- ch06/src/parse.rs
+++ ch06/src/parse.rs
@@ -8,20 +8,49 @@ pub enum Node {
     Or((Box<Node>, Box<Node>)),
 }
 
+// Operator's precedence is followings(from high to low):
+//   1. ()
+//   2. *, +, ?
+//   3.  (Concatenation)
+//   4. |
+//
+// Reference is here:
+//   https://pubs.opengroup.org/onlinepubs/9799919799/nframe.html
+//
+// or     = concat ("|" or)?
+// concat = star (star)*
+// star   = group "*"?
+// group  = '(' or ')'
+//        | 'a'..'z'
 pub fn parse(input: &str) -> Node {
     let mut chars = input.chars().peekable();
-    parse_expr(&mut chars)
+    parse_or(&mut chars)
 }
 
-fn parse_expr<I>(chars: &mut Peekable<I>) -> Node
+fn parse_or<I>(chars: &mut Peekable<I>) -> Node
+where
+    I: Iterator<Item = char>,
+{
+    let mut n = parse_concat(chars);
+    if let Some(c) = chars.peek() {
+        if *c == '|' {
+            chars.next();
+            n = Node::Or((Box::new(n), Box::new(parse_or(chars))))
+        }
+    }
+
+    n
+}
+
+fn parse_concat<I>(chars: &mut Peekable<I>) -> Node
 where
     I: Iterator<Item = char>,
 {
     let mut seq = vec![];
     while let Some(&c) = chars.peek() {
         match c {
-            ')' => break,
-            _ => seq.push(parse_atom(chars)),
+            ')' | '|' => break,
+            _ => seq.push(parse_star(chars)),
         }
     }
 
@@ -32,30 +61,34 @@ where
     }
 }
 
-fn parse_atom<I>(chars: &mut Peekable<I>) -> Node
+fn parse_star<I>(chars: &mut Peekable<I>) -> Node
 where
     I: Iterator<Item = char>,
 {
-    let n = match chars.next().unwrap() {
-        '(' => {
-            let n = parse_expr(chars);
-            assert_eq!(chars.next(), Some(')'));
-            n
-        }
-        c => Node::Char(c),
-    };
-
-    if let Some(&next) = chars.peek() {
-        if next == '*' {
+    let mut n = parse_group(chars);
+    if let Some(&c) = chars.peek() {
+        if c == '*' {
             chars.next();
-            return Node::Star(Box::new(n));
-        } else if next == '|' {
-            chars.next();
-            let rhs = parse_expr(chars);
-            return Node::Or((Box::new(n), Box::new(rhs)));
+            n = Node::Star(Box::new(n));
         }
     }
+
     n
+}
+
+fn parse_group<I>(chars: &mut Peekable<I>) -> Node
+where
+    I: Iterator<Item = char>,
+{
+    let c = chars.next().unwrap();
+    match c {
+        '(' => {
+            let n = parse_or(chars);
+            assert_eq!(chars.next().unwrap(), ')');
+            n
+        }
+        _ => Node::Char(c),
+    }
 }
 
 #[cfg(test)]
@@ -155,13 +188,20 @@ mod tests {
             ))
         );
 
-        // assert_eq!(
-        //     parse("ab|cd"),
-        //     Node::Seq(vec![
-        //         Node::Char('a'),
-        //         Node::Or((Box::new(Node::Char('b')), Box::new(Node::Char('c')))),
-        //         Node::Char('d')
-        //     ])
-        // );
+        assert_eq!(
+            parse("abc|def"),
+            Node::Or((
+                Box::new(Node::Seq(vec![
+                    Node::Char('a'),
+                    Node::Char('b'),
+                    Node::Char('c')
+                ])),
+                Box::new(Node::Seq(vec![
+                    Node::Char('d'),
+                    Node::Char('e'),
+                    Node::Char('f')
+                ]))
+            )),
+        );
     }
 }
