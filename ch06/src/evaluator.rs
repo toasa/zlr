6f8e74ff4387f5eef6324fb3406817e60e4eec40
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2024-09-23 16:44:13 +0900

    wip

--- ch06/src/evaluator.rs
+++ ch06/src/evaluator.rs
@@ -1,31 +1,49 @@
 use crate::codegen::Inst;
 
 pub fn eval(insts: Vec<Inst>, target: &str) -> bool {
-    let mut rm = Machine { pc: 0, sp: 0 };
-    rm.run(insts, target)
+    let mut rm = Machine {
+        pc: 0,
+        sp: 0,
+        insts: insts,
+        target_str: target.chars().collect(),
+    };
+    rm.run()
 }
 
 struct Machine {
     pc: usize, // program counter
     sp: usize, // string pointer
+
+    insts: Vec<Inst>,
+    target_str: Vec<char>,
 }
 
 impl Machine {
-    fn run(&mut self, insts: Vec<Inst>, target: &str) -> bool {
-        let chars: Vec<char> = target.chars().collect();
-
-        while self.pc < insts.len() {
-            match insts[self.pc] {
+    fn run(&mut self) -> bool {
+        while self.pc < self.insts.len() {
+            match self.insts[self.pc] {
                 Inst::Match => return true,
                 Inst::Char(c) => {
-                    if c == chars[self.sp] {
+                    if c == self.target_str[self.sp] {
                         self.pc += 1;
                         self.sp += 1
                     } else {
                         return false;
                     }
                 }
-                _ => return false,
+                Inst::Jmp(n) => self.pc = n,
+                Inst::Split(pc1, pc2) => {
+                    self.pc = pc1;
+                    let _sp = self.sp;
+                    if self.run() {
+                        return true;
+                    }
+
+                    self.pc = pc2;
+                    self.sp = _sp;
+
+                    return self.run();
+                }
             }
         }
         false
@@ -51,6 +69,20 @@ mod tests {
                 Inst::Match,
             ],
             "abc"
+        ));
+    }
+
+    #[test]
+    fn test_eval_or() {
+        assert!(eval(
+            vec![
+                Inst::Split(1, 3),
+                Inst::Char('a'),
+                Inst::Jmp(4),
+                Inst::Char('b'),
+                Inst::Match,
+            ],
+            "a"
         ));
     }
 }
